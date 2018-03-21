6978821ef343f4e99df538821eabe48485f12851
nbcallah <nbcallah@indiana.edu>
2018-03-21 11:18:21 -0400

    Removed an extraneous if check for neutron death and changed the death check to less than rather than greater

--- setup.h
+++ setup.h
@@ -1,28 +1,28 @@
 #define INITBLOCK 0
 
-#define TRACKGENERATOR randomPointTrapEdE
-//#define TRACKGENERATOR randomPointTrapOptimum
+//#define TRACKGENERATOR randomPointTrapEdE
+#define TRACKGENERATOR randomPointTrapOptimum
 
-#define TRACKER daggerHitTimes
-//#define TRACKER fixedEffDaggerHitTime
+//#define TRACKER daggerHitTimes
+#define TRACKER fixedEffDaggerHitTime
 
-#define WRITER writeNoabsRes
-//#define WRITER writeFixedRes
+//#define WRITER writeNoabsRes
+#define WRITER writeFixedRes
 
 #define SETTLINGTIME 150
 
-#define FIRSTDIPTIME 20
+#define FIRSTDIPTIME 1400
 
-#define HOLDTIME 20
+#define HOLDTIME 1400
 
 //9 Dip
-#define NDIPS 10
-#define HEIGHTS {0.49, 0.380, 0.250, 0.180, 0.140, 0.110, 0.080, 0.060, 0.040, 0.010}
-#define ENDTIMES {holdT,  holdT+40.0,  holdT+80.0,  holdT+100.0, holdT+120.0, holdT+140.0, holdT+160.0, holdT+180.0, holdT+200.0, holdT+300.0}
+//#define NDIPS 10
+//#define HEIGHTS {0.49, 0.380, 0.250, 0.180, 0.140, 0.110, 0.080, 0.060, 0.040, 0.010}
+//#define ENDTIMES {holdT,  holdT+40.0,  holdT+80.0,  holdT+100.0, holdT+120.0, holdT+140.0, holdT+160.0, holdT+180.0, holdT+200.0, holdT+300.0}
 
-//#define NDIPS 4
-//#define HEIGHTS {0.49, 0.380, 0.250, 0.010}
-//#define ENDTIMES {holdT, holdT+20.0, holdT+40.0, holdT+140.0}
+#define NDIPS 4
+#define HEIGHTS {0.49, 0.380, 0.250, 0.010}
+#define ENDTIMES {holdT, holdT+20.0, holdT+40.0, holdT+140.0}
 
 //#define NDIPS 12
 //#define HEIGHTS {0.49, 0.250, 0.49, 0.380, 0.250, 0.180, 0.140, 0.110, 0.080, 0.060, 0.040, 0.010}
--- src/trackUCN.cpp
+++ src/trackUCN.cpp
@@ -105,22 +105,8 @@ fixedResult fixedEffDaggerHitTime(std::vector<double> state, double dt) {
     res.eStart = res.eStart - MINU + (state[3]*state[3] + state[4]*state[4] + state[5]*state[5])/(2*MASS_N);
     
     double deathTime = -877.7*log(nextU01());
-    if(deathTime > 20) {
-            res.energy = res.eStart;
-            res.t = deathTime;
-            res.ePerp = state[4]*state[4]/(2*MASS_N);
-            res.x = state[0];
-            res.y = state[1];
-            res.z = state[2];
-            res.zOff = -1;
-            res.nHit = 0;
-            res.nHitHouseLow = 0;
-            res.nHitHouseHigh = 0;
-            res.deathTime = deathTime;
-            return res;
-    }
     
-    if(deathTime > FIRSTDIPTIME) {
+    if(deathTime < FIRSTDIPTIME) {
         res.energy = res.eStart;
         res.t = t;
         res.ePerp = state[4]*state[4]/(2*MASS_N);
