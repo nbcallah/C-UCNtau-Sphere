f3d5f84c8e7406b0d3f14068bbbdc6d003b76ed4
nbcallah <nbcallah@indiana.edu>
2018-04-19 17:05:23 -0400

    Added time constant to setup

--- setup.h
+++ setup.h
@@ -1,4 +1,4 @@
-#define INITBLOCK 0
+#define INITBLOCK 7
 
 //#define TRACKGENERATOR randomPointTrapEdE
 //#define TRACKGENERATOR randomPointTrapOptimum
@@ -18,9 +18,9 @@
 #define HOLDTIME 20
 
 //9 Dip
-#define NDIPS 10
-#define HEIGHTS {0.49, 0.380, 0.250, 0.180, 0.140, 0.110, 0.080, 0.060, 0.040, 0.010}
-#define ENDTIMES {holdT,  holdT+40.0,  holdT+80.0,  holdT+100.0, holdT+120.0, holdT+140.0, holdT+160.0, holdT+180.0, holdT+200.0, holdT+300.0}
+//#define NDIPS 10
+//#define HEIGHTS {0.49, 0.380, 0.250, 0.180, 0.140, 0.110, 0.080, 0.060, 0.040, 0.010}
+//#define ENDTIMES {holdT,  holdT+40.0,  holdT+80.0,  holdT+100.0, holdT+120.0, holdT+140.0, holdT+160.0, holdT+180.0, holdT+200.0, holdT+300.0}
 
 //#define NDIPS 4
 //#define HEIGHTS {0.49, 0.380, 0.250, 0.010}
@@ -29,6 +29,12 @@
 //#define NDIPS 12
 //#define HEIGHTS {0.49, 0.250, 0.49, 0.380, 0.250, 0.180, 0.140, 0.110, 0.080, 0.060, 0.040, 0.010}
 //#define ENDTIMES {0.0,  200.0,  200.0+holdT, 200.0+holdT+20.0, 200.0+holdT+40.0, 200.0+holdT+50.0, 200.0+holdT+60.0, 200.0+holdT+70.0, 200.0+holdT+80.0, 200.0+holdT+90.0, 200.0+holdT+100.0, 200.0+holdT+120.0}
+
+//dipHeights = (/0.49_8, 0.380_8, 0.250_8, 0.01_8/)
+//dipEnds =     (/0.0_8,  40.0_8,  400.0_8, 500.0_8/)
+#define NDIPS 10
+#define HEIGHTS {0.49, 0.38, 0.25, 0.18, 0.14, 0.11, 0.08, 0.06, 0.04, 0.01}
+#define ENDTIMES {holdT, holdT+40.0, holdT+80, holdT+100, holdT+120, holdT+140, holdT+160, holdT+180, holdT+200.0, holdT+500.0}
 
 #define XFNAME "/N/u/nbcallah/BigRed2/ChaoticTrap/C-UCNtau-Trap-Sims/xvals.bin"
 #define YFNAME "/N/u/nbcallah/BigRed2/ChaoticTrap/C-UCNtau-Trap-Sims/yvals.bin"
